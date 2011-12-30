2050322b8a6487d626d69f3a3610fedeae718045
Brant <brant.steen@gmail.com>
2011-12-30 14:57:19 -0500

    site context processors

--- noodles/context_processors.py
+++ noodles/context_processors.py
@@ -2,7 +2,7 @@
 Available context processors for django-noodles
 """
 from django.conf import settings
-
+from django.contrib.sites.models import Site
 from noodles.models import SiteMeta
 
 def static_paths(request):
@@ -13,6 +13,18 @@ def static_paths(request):
         "JS": "%s%s/" % (settings.STATIC_URL, "js"),
         "CSS": "%s%s/" % (settings.STATIC_URL, "css")
     }
+    
+def site(request):
+    """
+    Return site.name and site.domain as a URL
+    """
+    site = Site.objects.get_current()
+    return {
+        "SITE_NAME": site.name,
+        "SITE_URL": "http://%s/" % site.domain
+    }
+    
+    
 
 def site_meta(request):
     """
