e19b63d66b251c0c2d22e8f0316299a9b51040ee
Brant Steen <brant@Brant-Steens-MacBook-Pro.local>
2012-01-02 21:59:17 -0500

    Email input field

--- noodles/forms.py
+++ noodles/forms.py
@@ -3,15 +3,24 @@ Forms for Noodles
 """
 
 from django import forms
+from django.forms.widgets import Input
 from noodles.models import ContactSubmission
+
+
+class EmailInput(Input):
+    input_type = 'email'
 
 class ContactForm(forms.ModelForm):
     """
     Contact form, based on ContactSubmission model
     """
+    
+    email = forms.CharField(widget=EmailInput)
+    
     class Meta:
         """
         Django Metadata
         """
         model = ContactSubmission
         exclude = ["date"]
+        
