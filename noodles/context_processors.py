cef236e9b31fc49003836fab3421f345ad0205ed
Brant Steen <brant.steen@gmail.com>
2013-05-11 17:35:40 -0400

    Making the all noodles processor actually work..

--- noodles/context_processors.py
+++ noodles/context_processors.py
@@ -3,6 +3,7 @@ Available context processors for django-noodles
 """
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.db import DatabaseError
 
 from noodles.models import SiteMeta
 
@@ -22,11 +23,14 @@ def site(request):
     """
     Return site.name and site.domain as a URL
     """
-    THIS_SITE = Site.objects.get_current()
-    return {
-        "SITE_NAME": THIS_SITE.name,
-        "SITE_URL": "http://%s" % THIS_SITE.domain
-    }
+    try:
+        THIS_SITE = Site.objects.get_current()
+        return {
+            "SITE_NAME": THIS_SITE.name,
+            "SITE_URL": "http://%s" % THIS_SITE.domain
+        }
+    except DatabaseError:
+        return {}
 
 
 SITE_META = {}
@@ -37,14 +41,26 @@ def site_meta(request):
     available like this:
         {{ SITE_META.key }}
     """
-    META = SiteMeta.objects.all()
-    for data in META:
-        SITE_META.update({data.key: data.value})    
-    return {'SITE_META': SITE_META}
+    try:
+        META = SiteMeta.objects.all()
+        for data in META:
+            SITE_META.update({data.key: data.value})    
+        return {'SITE_META': SITE_META}
+    except DatabaseError:
+        return {}
 
 
 def noodle_processors(request):
     """
     ALL THE NOODLES!
     """
-    return site(request).update(site_meta(request)).update(static_paths(request))
+    site_pr = site(request)
+    meta_pr = site_meta(request)
+    static_pr = static_paths(request)
+    
+    ret = {}
+    ret.update(site_pr)
+    ret.update(meta_pr)
+    ret.update(static_pr)
+    
+    return ret
