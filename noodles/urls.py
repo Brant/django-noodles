8fa8f73f27373e26e4447a0b0c14b20e52616603
Brant Steen <brant.steen@gmail.com>
2013-06-01 11:12:31 -0400

    Favicon Patterns

--- noodles/urls.py
+++ noodles/urls.py
@@ -2,10 +2,16 @@
 Noodles URLs
 """
 from django.conf.urls import patterns, url
-
+from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('noodles.views',    
     url(r'^contact/$', 'contact', name='contact'),
     url(r'^contact/thanks/$', 'contact_thanks', name='contact_thanks'),
 )
 
+favicon_patterns = patterns('',
+    (r'^favicon\.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico'),
+    (r'^favicon\.png', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.png'),
+    (r'^apple-touch-icon\.png$', RedirectView.as_view(url=settings.STATIC_URL + 'apple-touch-icon.png'),
+)
