70cc8515ae59d9ebb7f8b7ceba88de1079f3562c
Brant Steen <brant.steen@gmail.com>
2013-06-02 17:52:51 -0400

    Way to go...

--- noodles_tests/runtests.py
+++ noodles_tests/runtests.py
@@ -19,7 +19,7 @@ if not settings.configured:
         ],
         SITE_ID = 1,
         STATIC_URL = '/static/',
-        MEDIA_URL = '/static/',
+        MEDIA_URL = '/media/',
         ROOT_URLCONF = 'noodles_tests.urls',
         NOSE_ARGS = ["--with-xcoverage", "--cover-inclusive", "--with-xunit", "--exe", "--verbosity=3", "--cover-package=noodles"],
         NOSE_PLUGINS = [
