221ed9e72268706ef6364548803cb22c22be3825
Brant Steen <brant.steen@gmail.com>
2014-06-30 21:50:59 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-noodles",
-    version = "0.2.0",
+    version = "0.2.1",
     url = 'http://github.com/Brant/django-noodles',
     license = 'GPL',
     description = "Half-baked noodles of ideas for little bits of functionality for Django",
