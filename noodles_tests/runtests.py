fb885d1f8b18fabba499a2b3245ebd21a99df1bd
Brant Steen <brant.steen@gmail.com>
2016-04-05 13:35:26 -0400

    updating for django 1.9

--- noodles_tests/runtests.py
+++ noodles_tests/runtests.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import sys
+import os
 
+import django
 from django.conf import settings
+from django.test.utils import get_runner
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
 
 if not settings.configured:
     settings.configure(
+        BASE_DIR = BASE_DIR,
         DATABASES={
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
@@ -14,30 +21,23 @@ if not settings.configured:
         INSTALLED_APPS=[
             'noodles',
             'noodles_tests',
+            'django.contrib.contenttypes',
+            'django.contrib.auth',
             'django.contrib.sites',
-            'django_nose'
         ],
         SITE_ID=1,
         STATIC_URL='/static/',
         MEDIA_URL='/media/',
         ROOT_URLCONF='noodles_tests.urls',
-        NOSE_ARGS=["--with-xcoverage", "--cover-inclusive", "--with-xunit", "--exe", "--verbosity=3", "--cover-package=noodles"],
-        NOSE_PLUGINS=[
-            'nosexcover.XCoverage',
-            "nose_exclude.NoseExclude"
-        ],
     )
 
 
-from django.test.simple import DjangoTestSuiteRunner
-from django_nose.runner import NoseTestSuiteRunner
-
-
 def runtests():
-    runner = DjangoTestSuiteRunner()
-    runner = NoseTestSuiteRunner()
-    failures = runner.run_tests(['noodles_tests'])
-    sys.exit(failures)
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["noodles_tests"])
+    sys.exit(bool(failures))
 
 if __name__ == '__main__':
     runtests(*sys.argv[1:])
