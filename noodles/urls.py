254c0cb96354343c626867b027aae25ddfa2abf3
Brant Steen <brant.steen@gmail.com>
2016-05-23 12:02:33 -0400

    cleaning up some url patterns

--- noodles/urls.py
+++ noodles/urls.py
@@ -4,14 +4,17 @@ Noodles URLs
 from django.conf.urls import patterns, url
 from django.conf import settings
 from django.views.generic import RedirectView
+from noodles.views import contact, contact_thanks
 
-urlpatterns = patterns('noodles.views',    
-    url(r'^contact/$', 'contact', name='contact'),
-    url(r'^contact/thanks/$', 'contact_thanks', name='contact_thanks'),
-)
 
-favicon_patterns = patterns('',
+urlpatterns = [
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^contact/thanks/$', contact_thanks, name='contact_thanks'),
+]
+
+
+favicon_patterns = [
     url(r'^favicon\.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico'), name="favicon-ico"),
     url(r'^favicon\.png', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.png'), name="favicon-png"),
     url(r'^apple-touch-icon\.png$', RedirectView.as_view(url=settings.STATIC_URL + 'apple-touch-icon.png'), name="apple-touch-png"),
-)
+]
